b201db4a535654eac90904a89e974fad4989c309
RithikaSelvakumar <150161075+RithikaSelvakumar@users.noreply.github.com>
2024-12-01 23:16:16 +0530

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -25,47 +25,9 @@ embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME')
 chunk_size = 100
 chunk_overlap = 10
 
-
-# Custom document loaders
-# class MyElmLoader(UnstructuredEmailLoader):
-#     """Wrapper to fallback to text/plain when default does not work"""
-
-#     def load(self) -> List[Document]:
-#         """Wrapper adding fallback for elm without html"""
-#         try:
-#             try:
-#                 doc = UnstructuredEmailLoader.load(self)
-#             except ValueError as e:
-#                 if 'text/html content not found in email' in str(e):
-#                     # Try plain text
-#                     self.unstructured_kwargs["content_source"]="text/plain"
-#                     doc = UnstructuredEmailLoader.load(self)
-#                 else:
-#                     raise
-#         except Exception as e:
-#             # Add file_path to exception message
-#             raise type(e)(f"{self.file_path}: {e}") from e
-
-#         return doc
-
-
 # Map file extensions to document loaders and their arguments
 LOADER_MAPPING = {
-    # ".csv": (CSVLoader, {}),
-    # # ".docx": (Docx2txtLoader, {}),
-    # ".doc": (UnstructuredWordDocumentLoader, {}),
-    # ".docx": (UnstructuredWordDocumentLoader, {}),
-    # ".enex": (EverNoteLoader, {}),
-    # ".eml": (MyElmLoader, {}),
-    # ".epub": (UnstructuredEPubLoader, {}),
-    # ".html": (UnstructuredHTMLLoader, {}),
-    # ".md": (UnstructuredMarkdownLoader, {}),
-    # ".odt": (UnstructuredODTLoader, {}),
     ".pdf": (PyMuPDFLoader, {}),
-    # ".ppt": (UnstructuredPowerPointLoader, {}),
-    # ".pptx": (UnstructuredPowerPointLoader, {}),
-    # ".txt": (TextLoader, {"encoding": "utf8"}),
-    # Add more mappings for other file extensions and loaders as needed
 }
 
 
